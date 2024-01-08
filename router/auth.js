ebca12b4c7a0427d2c8886b8371b3d8f2e1a136c
Abbas Mehdi <abbasmehdi692@gmail.com>
2024-01-09 03:51:45 +0500

    eblingo new database

--- router/auth.js
+++ router/auth.js
@@ -7,11 +7,12 @@ const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const sharp = require('sharp');
 const dayjs = require('dayjs');
+const path = require('path');
 
 
 const storageOriginal = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, 'uploads/originals/'));
+    cb(null, path.join(__dirname, 'uploads/'));
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
